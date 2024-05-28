98f9e4b2ff7ef33a9f56d664f79ff075942af712
Fazil <122675114+fazilcodes@users.noreply.github.com>
2024-05-28 12:18:32 +0530

    updated ai

--- ai.py
+++ ai.py
@@ -1,20 +1,3 @@
-# from openai import OpenAI
-# from dotenv import load_dotenv
-
-# load_dotenv()
-
-# client = OpenAI()
-
-# completion = client.chat.completions.create(
-#     model="gpt-3.5-turbo",
-#     messages=[
-#         {"role": "system", "content": "You are a short story creator expert about a character and its details and the story must be 4 to 5 sentence maximum"},
-#         {"role": "user", "content": "character name is Jaggu and the details is Monkey who is friends with people and loves eating laddu"}
-#     ]
-# )
-
-# print(completion)
-
 # --------------------------------GEMINI-------------------------------------
 
 import google.generativeai as genai
