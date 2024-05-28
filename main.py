27a7af83cd5908a2b76c66351b18b64c47c41417
Fazil <122675114+fazilcodes@users.noreply.github.com>
2024-05-28 13:54:01 +0530

    home interface

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from routers.characters import router as characters_router
 from routers.generate import router as generate_router
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/")
+def root(request: Request):
+    return templates.TemplateResponse("index.html", { "request": request })
+
 
 app.include_router(characters_router)
 app.include_router(generate_router)
